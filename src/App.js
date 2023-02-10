ce71d3b464e4a26b0ce0eea267dab8a01ca92ff7
Adaeze Chukwukereze <adaeze633@gmail.com>
2023-02-10 08:00:18 +0100

    upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import { useReactMediaRecorder } from "react-media-recorder";
+
+function App() {
+  const { status, startRecording, stopRecording, mediaBlobUrl } =
+    useReactMediaRecorder({ video: true });
+
+  return (
+    <div>
+      <p>{status}</p>
+      <button onClick={startRecording}>Start Recording</button>
+      <button onClick={stopRecording}>Stop Recording</button>
+      <video src={mediaBlobUrl} controls autoPlay loop />
+    </div>
+  );
+};
+
+
+export default App;
